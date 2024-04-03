1f6a542a07a7cf229935572566762b2474e5e9f7
a10mal <abbymalbon10@gmail.com>
2024-04-03 21:14:27 +1300

    Created a booking form and connected it to Mongodb

---
+++ Documents/Mock-up/figma.md
@@ -0,0 +1,2 @@
+* FIGMA MOCK-UP LINK
+https://www.figma.com/file/ubCe1ZGnVz7LEiLOk4InCr/Capstone_Website?type=design&node-id=0%3A1&mode=design&t=QgtDxQTP1sRum9Hc-1
---
+++ backend/models/booking.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  organisingTeacherName: String,
+  email: String,
+  mobileNumber: String,
+  schoolName: String,
+  address: String,
+  visitFormat: String,
+  firstPreferredDate: Date,
+  secondPreferredDate: Date,
+  thirdPreferredDate: Date
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
